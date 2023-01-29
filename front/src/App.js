8a7d96bcc7a5b45f02cf13253f337d38fd9616fe
Julick Mellah <j.mellah@icloud.com>
2023-01-29 21:00:46 +0100

    add socket.io in front

--- front/src/App.js
+++ front/src/App.js
@@ -9,17 +9,18 @@ import {
   Grid,
   theme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import io from "socket.io-client";
+
+
+const socket = io.connect("http://localhost:3001");
+
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
             <Text>
               Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
             </Text>
