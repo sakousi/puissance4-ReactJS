2754293f1338e3b0a67066528d8b553b78deb812
Julick Mellah <j.mellah@icloud.com>
2023-02-09 22:51:43 +0100

    init react app socket.io client tailwind css

--- front/src/App.js
+++ front/src/App.js
@@ -1,43 +1,29 @@
-import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
+import "./reset.css";
+import "./App.css";
+import "./index.css";
 import io from "socket.io-client";
-
-
 const socket = io.connect("http://localhost:3001");
-
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <VStack spacing={8}>
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </header>
+    </div>
   );
 }
 
 export default App;
+
