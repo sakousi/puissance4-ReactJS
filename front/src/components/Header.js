c9b1d599ebecb16467125616bc5230496be45e45
Julick Mellah <74454432+Melliche@users.noreply.github.com>
2024-01-30 12:21:55 +0100

    aller::eee:

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -13,7 +13,7 @@ export default function Header() {
     localStorage.removeItem("token");
     window.location.reload();
     client.resetStore();
-    console.log("on espere que ca marche cette fois ci c'dst bon");
+    console.log("on espere que ca marche cette fois ci c'dst bon 100%");
   };
 
   const handleClickOutside = (event) => {
